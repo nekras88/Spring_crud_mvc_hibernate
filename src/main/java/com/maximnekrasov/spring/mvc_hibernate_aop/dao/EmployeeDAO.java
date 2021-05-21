d189801fc812b8e07a1f6f28bc3ef0ad4a39a332
nekras88 <nekras88@mail.ru>
2021-05-21 14:28:52 +0300

    Create table

---
+++ src/main/java/com/maximnekrasov/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.maximnekrasov.spring.mvc_hibernate_aop.dao;
+
+import com.maximnekrasov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+
+    public List<Employee> getAllEmployees();
+}
